2079e2584ea5479e3e037a01458dbd9134a25f3f
Kaloyan Terziev <kaloyan.terziev@student.manchester.ac.uk>
2023-02-07 00:10:27 +0000

    Add 1 more unit test for TasksGraph

--- src/main/java/com/krterziev/jobprocessor/models/Task.java
+++ src/main/java/com/krterziev/jobprocessor/models/Task.java
@@ -1,6 +1,5 @@
 package com.krterziev.jobprocessor.models;
 
-import java.util.List;
 import java.util.Set;
 
 public record Task(String name, String command, Set<String> requires) {
--- src/test/java/com/krterziev/jobprocessor/scheduling/TasksGraphTest.java
+++ src/test/java/com/krterziev/jobprocessor/scheduling/TasksGraphTest.java
@@ -18,8 +18,10 @@ public class TasksGraphTest {
   private static final String TASK_1_NAME = "task-1";
   private static final String TASK_2_NAME = "task-2";
   private static final String TASK_3_NAME = "task-3";
-
   private static final String TASK_4_NAME = "task-4";
+  private static final String TASK_5_NAME = "task-5";
+  private static final String TASK_6_NAME = "task-6";
+  private static final String TASK_7_NAME = "task-7";
   private static final String TASK_COMMAND = "task command";
 
 
@@ -43,7 +45,8 @@ public class TasksGraphTest {
 
   @Test
   void givenUnsortedMultipleTasks_thenSortInOrder() throws CircularDependencyDetectedException {
-    final Set<String> taskThreePrerequisites = new HashSet<>(Arrays.asList(TASK_1_NAME, TASK_2_NAME));
+    final Set<String> taskThreePrerequisites = new HashSet<>(
+        Arrays.asList(TASK_1_NAME, TASK_2_NAME));
 
     final List<Task> tasks = Arrays.asList(
         new Task(TASK_3_NAME, TASK_COMMAND, taskThreePrerequisites),
@@ -62,7 +65,8 @@ public class TasksGraphTest {
 
   @Test
   void givenSortedMultipleTasks_thenSortInOrder() throws CircularDependencyDetectedException {
-    final Set<String> taskThreePrerequisites = new HashSet<>(Arrays.asList(TASK_1_NAME, TASK_2_NAME));
+    final Set<String> taskThreePrerequisites = new HashSet<>(
+        Arrays.asList(TASK_1_NAME, TASK_2_NAME));
 
     final List<Task> tasks = Arrays.asList(
         new Task(TASK_1_NAME, TASK_COMMAND, Collections.emptySet()),
@@ -83,7 +87,8 @@ public class TasksGraphTest {
   void givenMultipleTasksWithPrerequisitesContainingCircularDependency_thenSortInOrder() {
     final Set<String> taskTwoPrerequisites = new HashSet<>(Arrays.asList(TASK_3_NAME));
     final Set<String> taskThreePrerequisites = new HashSet<>(Arrays.asList(TASK_2_NAME));
-    final Set<String> taskFourPrerequisites = new HashSet<>(Arrays.asList(TASK_2_NAME, TASK_3_NAME));
+    final Set<String> taskFourPrerequisites = new HashSet<>(
+        Arrays.asList(TASK_2_NAME, TASK_3_NAME));
 
     final List<Task> tasks = Arrays.asList(
         new Task(TASK_1_NAME, TASK_COMMAND, Collections.emptySet()),
@@ -107,7 +112,8 @@ public class TasksGraphTest {
       throws CircularDependencyDetectedException {
     final Set<String> taskTwoPrerequisites = new HashSet<>(Arrays.asList(TASK_3_NAME));
     final Set<String> taskThreePrerequisites = new HashSet<>(Arrays.asList(TASK_1_NAME));
-    final Set<String> taskFourPrerequisites = new HashSet<>(Arrays.asList(TASK_2_NAME, TASK_3_NAME));
+    final Set<String> taskFourPrerequisites = new HashSet<>(
+        Arrays.asList(TASK_2_NAME, TASK_3_NAME));
 
     final List<Task> tasks = Arrays.asList(
         new Task(TASK_1_NAME, TASK_COMMAND, Collections.emptySet()),
@@ -131,7 +137,8 @@ public class TasksGraphTest {
       throws CircularDependencyDetectedException {
     final Set<String> taskTwoPrerequisites = new HashSet<>(Arrays.asList(TASK_1_NAME));
     final Set<String> taskThreePrerequisites = new HashSet<>(Arrays.asList(TASK_1_NAME));
-    final Set<String> taskFourPrerequisites = new HashSet<>(Arrays.asList(TASK_2_NAME, TASK_3_NAME));
+    final Set<String> taskFourPrerequisites = new HashSet<>(
+        Arrays.asList(TASK_2_NAME, TASK_3_NAME));
 
     final List<Task> tasks = Arrays.asList(
         new Task(TASK_1_NAME, TASK_COMMAND, Collections.emptySet()),
@@ -150,5 +157,39 @@ public class TasksGraphTest {
     assertThat(graph.sort(), equalTo(expectedTasks));
   }
 
+  @Test
+  void givenSevenTasksWithMultiplePrerequisites_thenSortInOrder()
+      throws CircularDependencyDetectedException {
+    final Set<String> taskTwoPrerequisites = new HashSet<>(Arrays.asList(TASK_1_NAME, TASK_7_NAME));
+    final Set<String> taskThreePrerequisites = new HashSet<>(
+        Arrays.asList(TASK_1_NAME, TASK_2_NAME));
+    final Set<String> taskFourPrerequisites = new HashSet<>(
+        Arrays.asList(TASK_6_NAME, TASK_3_NAME));
+    final Set<String> taskFivePrerequisites = new HashSet<>(
+        Arrays.asList(TASK_6_NAME));
+    final Set<String> taskSixPrerequisites = new HashSet<>(
+        Arrays.asList(TASK_7_NAME, TASK_2_NAME));
 
+    final List<Task> tasks = Arrays.asList(
+        new Task(TASK_1_NAME, TASK_COMMAND, Collections.emptySet()),
+        new Task(TASK_2_NAME, TASK_COMMAND, taskTwoPrerequisites),
+        new Task(TASK_3_NAME, TASK_COMMAND, taskThreePrerequisites),
+        new Task(TASK_4_NAME, TASK_COMMAND, taskFourPrerequisites),
+        new Task(TASK_5_NAME, TASK_COMMAND, taskFivePrerequisites),
+        new Task(TASK_6_NAME, TASK_COMMAND, taskSixPrerequisites),
+        new Task(TASK_7_NAME, TASK_COMMAND, Collections.emptySet()));
+
+    final TasksGraph graph = new TasksGraph(tasks);
+
+    final List<Task> expectedTasks = Arrays.asList(
+        new Task(TASK_1_NAME, TASK_COMMAND, Collections.emptySet()),
+        new Task(TASK_7_NAME, TASK_COMMAND, Collections.emptySet()),
+        new Task(TASK_2_NAME, TASK_COMMAND, Collections.emptySet()),
+        new Task(TASK_3_NAME, TASK_COMMAND, Collections.emptySet()),
+        new Task(TASK_6_NAME, TASK_COMMAND, Collections.emptySet()),
+        new Task(TASK_4_NAME, TASK_COMMAND, Collections.emptySet()),
+        new Task(TASK_5_NAME, TASK_COMMAND, Collections.emptySet()));
+
+    assertThat(graph.sort(), equalTo(expectedTasks));
+  }
 }
